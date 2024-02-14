76966e130fdbd53c03d93ac604466531e71c6abe
Vaanmathi24 <vaanmathi2401@gmail.com>
2024-02-14 14:21:26 +0530

    Initial commit

---
+++ src/main/java/com/example/hellogitpoject/HelloGitProjectApplication.java
@@ -0,0 +1,21 @@
+package com.example.hellogitpoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloGitProjectApplication {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, GIT!";
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloGitProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/hellogitpoject/HelloGitPojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellogitpoject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGitPojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
