f2b779025174a2aa4ff0b635c0fd1466ca9824eb
Vaanmathi24 <vaanmathi2401@gmail.com>
2024-02-14 14:25:30 +0530

    Your commit message

--- src/main/java/com/example/hellogitpoject/HelloGitProjectApplication.java
+++ src/main/java/com/example/hellogitpoject/HelloGitProjectApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class HelloGitProjectApplication {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello1")
     public String hello() {
         return "Hello, GIT!";
     }
